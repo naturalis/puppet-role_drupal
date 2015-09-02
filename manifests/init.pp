986b39e19f386de77f91180a1264887ca745cc0c
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2015-09-02 13:27:56 +0000

    updated version to 7.39

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,7 +12,7 @@ class role_drupal (
   $configuredrupal              = true,
   $dbpassword                   = 'password',
   $docroot                      = '/var/www/drupal',
-  $drupalversion                = '7.38',
+  $drupalversion                = '7.39',
   $updateall                    = false,        # all updates using drush up
   $updatesecurity               = true,         # only security updates
   $drushversion                 = '7.x-5.9',
