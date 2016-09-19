fa3a3d369d5dc3cc287a8923ad6fd726d2315206
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2016-09-19 14:51:12 +0000

    added update statements for old drush setups

--- manifests/update.pp
+++ manifests/update.pp
@@ -24,6 +24,20 @@ class role_drupal::update (
       user      => 'root',
       onlyif    => 'drush pm-updatestatus | grep -c "SECURITY"'
     }
+    exec { 'update_all_old_drush':
+      command   => 'drush upc -u 1 --pipe | grep "Update-available" | cut -d" " -f1 | xargs drush up -u 1 -y',
+      path      => '/sbin:/usr/bin:/usr/local/bin/:/bin/',
+      provider  => shell,
+      user      => 'root',
+      onlyif    => 'drush upc -u 1 --pipe | grep -c "Update-available"'
+    }
+    exec { 'update_security_old_drush':
+      command   => 'drush upc -u 1 --pipe | grep "Update-available" | cut -d" " -f1 | xargs drush up -u 1 -y',
+      path      => '/sbin:/usr/bin:/usr/local/bin/:/bin/',
+      provider  => shell,
+      user      => 'root',
+      onlyif    => 'drush upc -u 1 --pipe | grep -c "SECURITY-UPDATE"'
+    }
   } else {
     exec { 'update_security':
       command   => 'drush pm-updatestatus --full | grep "SECURITY" | cut -d" " -f2 | xargs drush up -u 1 -y',
@@ -32,7 +46,15 @@ class role_drupal::update (
       user      => 'root',
       onlyif    => 'drush pm-updatestatus | grep -c "SECURITY"'
     }
+    exec { 'update_security_old_drush':
+      command   => 'drush upc -u 1 --pipe | grep "Update-available" | cut -d" " -f1 | xargs drush up -u 1 -y',
+      path      => '/sbin:/usr/bin:/usr/local/bin/:/bin/',
+      provider  => shell,
+      user      => 'root',
+      onlyif    => 'drush upc -u 1 --pipe | grep -c "SECURITY-UPDATE"'
+    }
   }
+
 
 # update composer and drush when new drush version is available
   if ( $updatedrush == true ) {
