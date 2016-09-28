d9913375ae0badc04d64ea8681e83c6f3ecdc094
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2016-09-28 09:05:57 +0000

    updated security update routines and support for installing with default profile

---
+++ manifests/updateold.pp
@@ -0,0 +1,36 @@
+# == Class: role_drupal::updateold
+#
+# === Authors
+#
+# Author Name <hugo.vanduijn@naturalis.nl>
+#
+#
+class role_drupal::updateold (
+){
+
+  if ( $role_drupal::updateall == true ) {
+    exec { 'update_all_old_drush':
+      command   => 'drush upc -u 1 --pipe | grep "Update-available" | cut -d" " -f1 | xargs drush up -u 1 -y',
+      path      => '/sbin:/usr/bin:/usr/local/bin/:/bin/',
+      provider  => shell,
+      user      => 'root',
+      onlyif    => 'drush upc -u 1 --pipe | grep -c "Update-available"'
+    }
+    exec { 'update_security_old_drush':
+      command   => 'drush upc -u 1 --pipe | grep "Update-available" | cut -d" " -f1 | xargs drush up -u 1 -y',
+      path      => '/sbin:/usr/bin:/usr/local/bin/:/bin/',
+      provider  => shell,
+      user      => 'root',
+      onlyif    => 'drush upc -u 1 --pipe | grep -c "SECURITY-UPDATE"'
+    }
+  } else {
+    exec { 'update_security_old_drush':
+      command   => 'drush upc -u 1 --pipe | grep "Update-available" | cut -d" " -f1 | xargs drush up -u 1 -y',
+      path      => '/sbin:/usr/bin:/usr/local/bin/:/bin/',
+      provider  => shell,
+      user      => 'root',
+      onlyif    => 'drush upc -u 1 --pipe | grep -c "SECURITY-UPDATE"'
+    }
+  }
+
+}
