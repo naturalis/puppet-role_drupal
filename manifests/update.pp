f4ae722a9f793a478a0210043396a248e49d31a3
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2018-11-19 09:33:38 +0000

    fixed update commands

--- manifests/update.pp
+++ manifests/update.pp
@@ -10,29 +10,29 @@ class role_drupal::update (
 
   if ( $role_drupal::updateall == true ) {
     exec { 'update_all':
-      command   => 'docker-compose exec drupal drush "pm-updatestatus --full" | grep "Update available" | cut -d" " -f2 | xargs drush up --no-backup -u 1 -y',
+      command   => 'docker-compose exec -T drupal drush pm-updatestatus --full | grep "Update available" | cut -d" " -f2 | xargs drush up --no-backup -u 1 -y',
       path      => '/sbin:/usr/bin:/usr/local/bin/:/bin/',
       cwd       => $role_drupal::repo_dir,
       provider  => shell,
       user      => 'root',
-      onlyif    => 'docker-compose exec drupal drush pm-updatestatus | grep -c "Update available"'
+      onlyif    => 'docker-compose exec -T drupal drush pm-updatestatus | grep -c "Update available"'
     }
     exec { 'update_security':
-      command   => 'docker-compose exec drupal drush "pm-updatestatus --full" | grep "SECURITY" | cut -d" " -f2 | xargs docker-compose exec drupal drush up --no-backup -u 1 -y',
+      command   => 'docker-compose exec -T drupal drush pm-updatestatus --full | grep "SECURITY" | cut -d" " -f2 | xargs docker-compose exec -T drupal drush up --no-backup -u 1 -y',
       path      => '/sbin:/usr/bin:/usr/local/bin/:/bin/',
-#      provider  => shell,
+      provider  => shell,
       cwd       => $role_drupal::repo_dir,
       user      => 'root',
-      onlyif    => 'docker-compose exec drupal drush pm-updatestatus | grep -c "SECURITY"'
+      onlyif    => 'docker-compose exec -T drupal drush pm-updatestatus | grep -c "SECURITY"'
     }
   } else {
     exec { 'update_security':
-      command   => 'docker-compose exec drupal drush "pm-updatestatus --full" | grep "SECURITY" | cut -d" " -f2 | xargs docker-compose exec drupal drush up --no-backup -u 1 -y',
+      command   => 'docker-compose exec -T drupal drush pm-updatestatus --full | grep "SECURITY" | cut -d" " -f2 | xargs docker-compose exec -T drupal drush up --no-backup -u 1 -y',
       path      => '/sbin:/usr/bin:/usr/local/bin/:/bin/',
       cwd       => $role_drupal::repo_dir,
       provider  => shell,
       user      => 'root',
-      onlyif    => 'docker-compose exec drupal drush pm-updatestatus | grep -c "SECURITY"'
+      onlyif    => 'docker-compose exec -T drupal drush pm-updatestatus | grep -c "SECURITY"'
     }
   }
 
