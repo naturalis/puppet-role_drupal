1e09fe2fd058127a2553a2e736f3107d1eac70a8
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2019-04-24 13:02:00 +0000

    update changes

--- manifests/update.pp
+++ manifests/update.pp
@@ -10,7 +10,7 @@ class role_drupal::update (
 
   if ( $role_drupal::updateall == true ) {
     exec { 'update_all':
-      command   => 'docker-compose exec -T drupal drush pm-updatestatus --full | grep "Update available" | cut -d" " -f2 | xargs drush up --no-backup -u 1 -y',
+      command   => 'docker-compose exec -T drupal drush up --no-backup -y',
       path      => '/sbin:/usr/bin:/usr/local/bin/:/bin/',
       cwd       => $role_drupal::repo_dir,
       provider  => shell,
@@ -18,7 +18,7 @@ class role_drupal::update (
       onlyif    => 'docker-compose exec -T drupal drush pm-updatestatus | grep -c "Update available"'
     }
     exec { 'update_security':
-      command   => 'docker-compose exec -T drupal drush pm-updatestatus --full | grep "SECURITY" | cut -d" " -f2 | xargs docker-compose exec -T drupal drush up --no-backup -u 1 -y',
+      command   => 'docker-compose exec -T drupal drush up --no-backup -y',
       path      => '/sbin:/usr/bin:/usr/local/bin/:/bin/',
       provider  => shell,
       cwd       => $role_drupal::repo_dir,
@@ -27,25 +27,12 @@ class role_drupal::update (
     }
   } else {
     exec { 'update_security':
-      command   => 'docker-compose exec -T drupal drush pm-updatestatus --full | grep "SECURITY" | cut -d" " -f2 | xargs docker-compose exec -T drupal drush up --no-backup -u 1 -y',
+      command   => 'docker-compose exec -T drupal drush up --no-backup -y',
       path      => '/sbin:/usr/bin:/usr/local/bin/:/bin/',
-      cwd       => $role_drupal::repo_dir,
       provider  => shell,
+      cwd       => $role_drupal::repo_dir,
       user      => 'root',
       onlyif    => 'docker-compose exec -T drupal drush pm-updatestatus | grep -c "SECURITY"'
     }
   }
-
-
-# update composer and drush when new drush version is available
-#  if ( $role_drupal::updatedrush == true ) {
-#    exec { 'update drush':
-#      command     => 'docker-compose exec drupal composer global self-update & composer global update',
-#      path        => '/sbin:/usr/bin:/usr/local/bin/:/bin/',
-#      cwd       => $role_drupal::repo_dir,
-#      cwd         => '/root/.composer/vendor/drush/drush',
-#      environment => ['HOME=/root'],
-#      onlyif      => 'docker-compose exec drupal composer global show -o | grep -c "drush"'
-#    }
-#  }
 }
